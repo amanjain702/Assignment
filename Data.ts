1cf40a4258be6de73934b9ecbac60161d87ba02d
amanjain702 <44317262+amanjain702@users.noreply.github.com>
2020-12-23 23:10:37 +0530

    Add files via upload

---
+++ Data.ts
@@ -0,0 +1,10 @@
+ data.ts file(typescript file)
+
+export const TEST_DATA = {
+  
+  "PUBLICATION_DATE": "10/30/2020",
+  "OBJECT": "Price Registration for a period of 12 months, for the purchase of galvanized clamps.",
+  "BIDDING_DATE": "10/30/2020",
+  "link":"Click Here "
+  
+}
---
+++ index.ts
@@ -0,0 +1,43 @@
+
+test component have html and ts file in it
+
+1. test.component.ts( main typescript file in component)
+
+import { TEST_DATA } from './data';
+
+allvalues:anyoo;
+
+	const()
+
+	ngonit()
+	{
+	function(); 
+	}
+
+
+	function()
+	{
+	this.allvalues=this.TEST_DATA;
+	}
+
+	
+ 2. test.component.html	(html file with inline css styling)
+
+<html>
+<head>
+<style>
+h3{
+color:dark-grey
+text-color: grey;
+}
+</style>
+</head>
+<body>
+
+<h3>PUBLICATION DATE: <h3>{{allvalues.PUBLICATION_DATE}}
+<h3>OBJECT: <h3>{{allvalues.OBJECT}}
+<h3>BIDDING DATE: <h3>{{allvalues.BIDDING_DATE}}
+<a href ="" >{{allvalues.link}}}</a>
+
+</body>
+</html>
